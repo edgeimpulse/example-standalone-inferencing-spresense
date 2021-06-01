06223c2a45c0948a552875e8d469dfd28bdd7308
automatiek <arjan.kamphuis@gmail.com>
2021-06-01 11:06:44 +0200

    Try to remove exern c ref

--- main.cpp
+++ main.cpp
@@ -4,26 +4,30 @@
 
 #include "ei_device_sony_spresense.h"
 
+/* Extern reference -------------------------------------------------------- */
 extern int ei_main();
 
-
-
-extern "C"
-{
-int spresense_main(void)
+/**
+ * @brief Main application function
+ *
+ */
+extern "C" int spresense_main(void)
 {
     boardctl(BOARDIOC_INIT, 0);
-
     ei_main();
     return 0;
 }
 
-void spresense_time_cb(uint32_t *sec, uint32_t *nano)
+/**
+ * @brief Get current time from spresense lib
+ *
+ * @param sec
+ * @param nano
+ */
+extern "C" void spresense_time_cb(uint32_t *sec, uint32_t *nano)
 {
     struct timespec cur_time;
     clock_gettime(CLOCK_MONOTONIC, &cur_time);
     *(sec) = cur_time.tv_sec;
     *(nano)= cur_time.tv_nsec;
-}
-
 }
